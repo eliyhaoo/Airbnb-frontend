8cdebf16a63704747f3fd38832c0cbeb8d9c3dcf
Amit Doanias <amitdoanias@gmail.com>
2022-05-25 13:56:46 +0300

    some details sass

--- src/cmps/stay-list.jsx
+++ src/cmps/stay-list.jsx
@@ -1,10 +1,9 @@
 import { StayPreview } from './stay-preview'
 
 
-export const StayList = ({stays}) => {
-    console.log('stays',stays);
+export const StayList = ({ stays }) => {
     return <section className="stay-list">
 
-        {stays.map(stay=><StayPreview key={stay._id} stay={stay}/>)} 
+        {stays.map(stay => <StayPreview key={stay._id} stay={stay} />)}
     </section>
 }
