b1322549b26eee7e7837a896f89702ee4377514d
Amit Doanias <amitdoanias@gmail.com>
2022-05-25 21:10:51 +0300

    some scss

--- src/cmps/details-cmps/stay-reserve.jsx
+++ src/cmps/details-cmps/stay-reserve.jsx
@@ -2,7 +2,7 @@
 export const StayReserve = () => {
 
     return <div className="stay-reserve">
-        <h2>HELLO FROM STAY RESERVE</h2>
+        {/* <h2>HELLO FROM STAY RESERVE</h2> */}
 
     </div>
 }
