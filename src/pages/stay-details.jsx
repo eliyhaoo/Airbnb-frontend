663b1f1156c4baf6a6e54b4446d24c299560d6f0
Amit Doanias <amitdoanias@gmail.com>
2022-05-25 12:43:18 +0300

    names changes

--- src/pages/stay-details.jsx
+++ src/pages/stay-details.jsx
@@ -1,10 +1,23 @@
+import { useEffect } from "react"
+import { useParams } from 'react-router-dom'
+
 import { MapStay } from "../cmps/details-cmps/map-stay"
 import { ReserveStay } from "../cmps/details-cmps/reserve-stay"
 import { ReviewList } from "../cmps/details-cmps/review-list"
+
+
 export const StayDetails = () => {
+
+
+    const { stayId } = useParams()
+
+    // useEffect(() => {
+    //     const stay = 
+    // })
 
     return <section className="stay-details">
         <h2>HELLO FROM STAY DETAILS</h2>
+
         <ReserveStay />
         <ReviewList />
         <MapStay />
