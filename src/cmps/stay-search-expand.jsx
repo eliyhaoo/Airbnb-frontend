f9d482185722f339922e51ced52ec9b7c4e165df
eliyhaoo <102289745+eliyhaoo@users.noreply.github.com>
2022-05-28 20:21:29 +0300

    deleted date-picker

--- src/cmps/stay-search-expand.jsx
+++ src/cmps/stay-search-expand.jsx
@@ -1,65 +1,145 @@
 import searchSvg from '../assets/svg/searchsvg.svg'
+import { SearchCountry } from './search-country'
 
 export const StaySearchExpand = () => {
 
 
-    return <section className="stay-search-expand flex space-between align-center">
+    return <section className="stay-search-expand ">
 
-        <div className="search-location-expand">
+        <div className="stay-search-expand-container flex space-between align-center">
+        <div className="search-location-expand open">
             <div>Where</div>
             <input type="text" placeholder="Search-destinations" />
+            
         </div>
         <div className="search-date-expand">
             <div>When</div>
             <span>Any week</span>
-            
+
         </div>
         <div className="search-guest-expand flex space-between align-center">
             <div>
-            <div>Who</div>
-            <span>Add guest</span>
+                <div>Who</div>
+                <span>Add guest</span>
 
             </div>
 
+            <div className="search-btn-container src-btn-big-expand">
 
-            <button className="src-btn-big-expand flex align-center" ><img src={searchSvg} alt="btn" />Search </button>
-            {/* <button className="search-btn-small-expand flex align-center" ><img src={searchSvg} alt="btn" /></button> */}
+                <div class="btn-container">
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="content">
+                        <button className="action-btn ">
+                            <img src={searchSvg} alt="btn" /><span></span>
+                        </button>
+                    </div>
+                </div>
+
             </div >
-
-            
-
-
-
-            {/* <div className="btn-search-expand-container"> */}
-
-                {/* <button className="src-btn-expand-container">
-                <img src={searchSvg} alt="btn" />
-                </button> */}
-            {/* </div> */}
-
-            {/* <div className="search-guest-expand flex space-between">
-            <div>Guests</div>
-            <div className="src-btn-container">
-                <button className="search-btn-expand flex align-center" ></button>
-            </div> */}
-
-        {/* 
-    <button  className="search-location">
-        AnyWhere
-    </button>
-
-    <button className="search-date">
-        Any Week
-    </button>
-
-    <button className="search-guest">
-        <div className="flex space-between align-center">
-            <div className="search-guest-btn">
-        Add guests 
-            </div>
-    <button className="search-btn flex align-center" ><img src={searchSvg} alt="btn" /> </button>
+        </div >
         </div>
-    </button> */}
+        {/* <SearchCountry/> */}
+           
+
+
 
 
     </section >
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,4 +1,3 @@
-import { DatePicker } from '../cmps/date-picker'
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { setIsInHomePage } from "../store/actions/system.action"
@@ -16,7 +15,6 @@ export const HomePage = () => {
             {/* <img src="https://a0.muscache.com/im/pictures/miso/Hosting-5904771/original/ab9a30d4-a6cf-4b3a-8416-cf7314ed5432.jpeg?im_w=1200" alt="hero" /> */}
         </div>
 
-        {/* <DatePicker/> */}
         {/*         
         <div className="grid-test main-">
        
