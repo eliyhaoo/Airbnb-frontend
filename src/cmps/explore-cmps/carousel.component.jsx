75fefbf76cf0c6d700fdd74cd42f4d7eb9bc100d
AdiZur <adizur9@gmail.com>
2022-05-29 00:31:25 +0300

    carousel

--- src/cmps/explore-cmps/carousel.component.jsx
+++ src/cmps/explore-cmps/carousel.component.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-// function onChange(event) {
-//     console.log('event', event)
-// }
-//  onChange={(event) => onChange(event)}
 export function CarouselComponent({ stayImgUrls }) {
 
     if (!stayImgUrls) return <div>Loading...</div>
     return (
         <div className="carousel-component">
-            <Carousel showArrows={true} showThumbs={false} infiniteLoop={true} showStatus={false} >
+            <Carousel showArrows={true} showThumbs={false} showStatus={false} useKeyboardArrows={true}
 
+            // renderArrowPrev={(onClickHandler, hasPrev, label) =>
+            //     hasPrev && (
+            //         <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, left: 15 }}>
+            //             -
+            //         </button>
+            //     )
+            // }
+            // renderArrowNext={(onClickHandler, hasNext, label) =>
+            //     hasNext && (
+            //         <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, right: 15 }}>
+            //             +
+            //         </button>
+            //     )
+            // }
+            >
                 {stayImgUrls.map((img, idx) =>
                     <div key={idx}>
                         <img className="preview-img" src={img} />
