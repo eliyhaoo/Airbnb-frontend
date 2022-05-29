5f14bf3e6dec2dc45d9680eae735e159957af80b
AdiZur <adizur9@gmail.com>
2022-05-29 17:29:22 +0300

    explore-preview

--- src/cmps/stay-preview.jsx
+++ src/cmps/stay-preview.jsx
@@ -1,14 +1,24 @@
+import starSvg from '../assets/svg/star.svg'
 import { Link } from "react-router-dom";
 import { CarouselComponent } from "./explore-cmps/carousel.component";
 export const StayPreview = ({ stay }) => {
+
+
+
+    const getPriceWithCommas = (price) => {
+        return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
     return <div className="stay-preview flex direction-column">
         <CarouselComponent stayImgUrls={stay.imgUrls} />
         <Link to={`/stay/${stay._id}`}><div className="preview-details-container">
             <p className="preview-country-city">{stay.address.city}, {stay.address.country}</p>
             <p className="preview-room-type">{stay.roomType}</p>
             <p className="preview-beds">{stay.beds} beds</p>
-            <p className="preview-rating">{stay.reviewScores.rating}★</p>
-            <p className="preview-price"><span className="price-span">${stay.price}</span> night</p>
+            <div className="preview-rating-container flex align-center space-between">
+                <p className="preview-rating">{stay.reviewScores.rating.toFixed(1)}</p><img className="star-svg" src={starSvg} />
+            </div>
+            <p className="preview-price"><span className="price-span">${getPriceWithCommas(stay.price)}</span> night</p>
         </div></Link>
     </div >
 }
