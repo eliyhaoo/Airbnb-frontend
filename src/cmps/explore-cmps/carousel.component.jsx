6e24b95e5d5923c885aa200ef6b5a3eeb73d2273
AdiZur <adizur9@gmail.com>
2022-05-29 12:10:09 +0300

    carousel

--- src/cmps/explore-cmps/carousel.component.jsx
+++ src/cmps/explore-cmps/carousel.component.jsx
@@ -1,69 +1,12 @@
 import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-
-
-// function renderArrowPrev() {
-//     // console.log(props)
-
-//     // const { className, style, onClick } = props
-//     // console.log(className, style, onClick)
-//     return (
-//         <div
-//             className="control-arrow"
-//             style={{ ...style, display: 'block' }}
-//             onClick={onClick}
-//         >
-//             {/* <img src={ARROW_left} alt="arrow_left" /> */}
-//             <button type="button"  >
-//                 -
-//             </button>
-//         </div>
-//     )
-// }
-
-// function renderArrowNext() {
-//     // console.log(props)
-
-//     // const { className, style, onClick } = props
-//     // console.log(className, style, onClick)
-
-//     return (
-//         <div
-//             className="control-arrow"
-//             style={{ ...style, display: 'block' }}
-//             onClick={onClick}
-//         >
-//             {/* <img src={ARROW_left} alt="arrow_left" /> */}
-//             <button type="button"  >
-//                 +
-//             </button>
-//         </div>
-//     )
-// }
-
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export function CarouselComponent({ stayImgUrls }) {
 
     if (!stayImgUrls) return <div>Loading...</div>
     return (
         <div className="carousel-component">
-            <Carousel showArrows={true} showThumbs={false} showStatus={false} useKeyboardArrows={true}
-            // renderArrowNext={renderArrowNext} renderArrowPrev={renderArrowPrev}
-
-            // renderArrowPrev={(onClickHandler, hasPrev, label) =>
-            //     hasPrev && (
-            //         <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, left: 15 }}>
-            //             -
-            //         </button>
-            //     )
-            // }
-            // renderArrowNext={(onClickHandler, hasNext, label) =>
-            //     hasNext && (
-            //         <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, right: 15 }}>
-            //             +
-            //         </button>
-            //     )
-            // }
+            <Carousel showThumbs={false} showStatus={false}
             >
                 {
                     stayImgUrls.map((img, idx) =>
