99c377f2bb3cb6021e49770d91c32a598c33d16c
AdiZur <adizur9@gmail.com>
2022-05-31 20:22:31 +0300

    footer

--- src/cmps/app-footer.jsx
+++ src/cmps/app-footer.jsx
@@ -1,11 +1,47 @@
+import { useSelector } from 'react-redux'
 import languageIcon from '../assets/svg/language-icon.svg'
 export const AppFooter = () => {
 
-    return <section className="app-footer full">
-        <div className="main-layout">
+    const { visitedPage } = useSelector(storeState => storeState.systemModule)
+
+    return <section className={("app-footer full " + visitedPage)}>
+        <div className={visitedPage === 'details-page' ? 'details-layout' : visitedPage === 'explore-page' ? 'main-layout' : 'home-page-layout'}>
+            {visitedPage === 'details-page' && <div class="details-page-footer-content">
+                <ul className="details-footer-support clean-list">
+                    <li>Help Center</li>
+                    <li>Supporting people with disabilities</li>
+                    <li>Report a neighborhood concern</li>
+                    <li>AirCover</li>
+                    <li>Cancellation options</li>
+                    <li>Safety information</li>
+                    <li>Our COVID-19 Response</li>
+                </ul>
+                <ul className="details-footer-community clean-list">
+                    <li>Help Center</li>
+                    <li>Airbnb.org: disaster relief housing</li>
+                    <li>Support Afghan refugees</li>
+                    <li>Combating discrimination</li>
+                </ul>
+                <ul className="details-footer-hosting clean-list">
+                    <li>Try hosting</li>
+                    <li>Visit our community forum</li>
+                    <li>AirCover for Hosts</li>
+                    <li>How to host responsibly</li>
+                    <li>Explore hosting resources</li>
+                    <li>Help Center</li>
+                </ul>
+                <ul className="details-footer-about clean-list">
+                    <li>Newsroom</li>
+                    <li>Careersr</li>
+                    <li>Learn about new features</li>
+                    <li>Investors</li>
+                    <li>Letter from our founders</li>
+                </ul>
+            </div>}
+            {/* <section className=> */}
             <div className="footer-content flex align-center space-between">
                 <div className="footer-right-side flex align-center space-between">
-                    <span>© 2022 AAEbnb,</span> <span>Inc.</span><span> ·</span><span>Privacy</span><span> · </span><span>Terms</span><span>·</span> <span>Sitemap</span>
+                    <span>© 2022 Topbnb,</span> <span>Inc.</span><span> ·</span><span>Privacy</span><span> · </span><span>Terms</span><span>·</span> <span>Sitemap</span>
                 </div>
                 <div className="footer-left-side flex align-center space-between">
                     <img className="language-icon" src={(languageIcon)} alt="language-icon" />
@@ -16,5 +52,8 @@ export const AppFooter = () => {
             </div>
         </div>
     </section >
+    // </section>
 }
+
+
 
--- src/cmps/stay-preview.jsx
+++ src/cmps/stay-preview.jsx
@@ -1,22 +1,20 @@
 import starSvg from '../assets/svg/star.svg'
 import { Link } from "react-router-dom";
 import { CarouselComponent } from "./explore-cmps/carousel.component";
-export const StayPreview = ({ stay, history }) => {
+import { utilService } from '../services/util.service';
 
-    const getPriceWithCommas = (price) => {
-        return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+export const StayPreview = ({ stay, history }) => {
 
     return <div className="stay-preview flex direction-column">
         <CarouselComponent stayImgUrls={stay.imgUrls} stayId={stay._id} history={history} />
         <Link to={`/stay/${stay._id}`}><div className="preview-details-container">
             <p className="preview-country-city">{stay.address.city}, {stay.address.country}</p>
             <p className="preview-room-type">{stay.roomType}</p>
-            <p className="preview-beds">{stay.beds} beds</p>
+            <p className="preview-beds">{utilService.checkForPlurals('bed', stay.beds)}</p>
             <div className="preview-rating-container flex align-center space-between">
                 <p className="preview-rating">{stay.reviewScores.rating.toFixed(1)}</p><img className="star-svg" src={starSvg} />
             </div>
-            <p className="preview-price"><span className="price-span">${getPriceWithCommas(stay.price)}</span> night</p>
+            <p className="preview-price"><span className="price-span">${utilService.getPriceWithCommas(stay.price)}</span> night</p>
         </div></Link>
     </div >
 }
