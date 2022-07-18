a78286d0c8174a087faa75307b4c65f8fb7d1dbd
Amit Doanias <amitdoanias@gmail.com>
2022-07-18 12:33:08 +0300

    getting changes

--- src/cmps/general-cmps/mobile-menu.jsx
+++ src/cmps/general-cmps/mobile-menu.jsx
@@ -8,26 +8,32 @@ export const MobileMenu = () => {
 
     // const { user } = useSelector(storeState => storeState.userModule)
 
-
     return <div className="mobile-menu flex direction-column align-center">
         <div className="mobile-menu-elements-container flex">
-            <Link to='/explore'><div className="mobile-element-container flex direction-column align-center">
-                <img className="search-img" src={searchSvg} />
-                <span className="name">Explore</span>
-            </div></Link>
-            <Link to="/dashboard/trip"><div className="mobile-element-container flex direction-column align-center">
-                <img className="travels-img" src={travelsSvg} />
-                <span className="name">Travels</span>
-            </div></Link>
-            <Link to="/dashboard/wishlist"><div className="mobile-element-container flex direction-column align-center">
-                <img className="wishList-img" src={wishListSvg} />
-                <span className="name">WishList</span>
-            </div></Link>
-            <Link to="/login"><div className="mobile-element-container flex direction-column align-center">
-                <img className="Profile-img" src={profileSvg} />
-                <span className="name">Log in</span>
-
-            </div></Link>
+            <Link to='/explore'>
+                <div className="mobile-element-container flex direction-column align-center">
+                    <img className="search-img" src={searchSvg} />
+                    <span className="name">Explore</span>
+                </div>
+            </Link>
+            <Link to="/dashboard/trip">
+                <div className="mobile-element-container flex direction-column align-center">
+                    <img className="travels-img" src={travelsSvg} />
+                    <span className="name">Travels</span>
+                </div>
+            </Link>
+            <Link to="/dashboard/wishlist">
+                <div className="mobile-element-container flex direction-column align-center">
+                    <img className="wishList-img" src={wishListSvg} />
+                    <span className="name">WishList</span>
+                </div>
+            </Link>
+            <Link to="/login">
+                <div className="mobile-element-container flex direction-column align-center">
+                    <img className="Profile-img" src={profileSvg} />
+                    <span className="name">Log in</span>
+                </div>
+            </Link>
         </div>
     </div>
 }
