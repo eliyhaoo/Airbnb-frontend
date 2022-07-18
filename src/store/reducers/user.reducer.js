37524ccbb3001012dd320b8bdf1a84c6f3fc9f71
Amit Doanias <amitdoanias@gmail.com>
2022-07-18 16:42:34 +0300

    last changes

--- src/store/reducers/user.reducer.js
+++ src/store/reducers/user.reducer.js
@@ -7,12 +7,10 @@ export function userReducer(state = initialState, action) {
     var newState = state;
     switch (action.type) {
         case 'SET_USER':
-            newState = { ...state, user: action.user }
+            newState = { user: action.user }
             break
         case 'UPDATE_USER_WISHLIST':
-            // newState = { ...state, user: action.user }
-            // newState = { ...state.user, wishList: action.wishList }
-            newState = { ...state.user, user: action.user }
+            newState = { user: action.user }
             break
         default:
     }
